97b160ed4880bce1782f944998a19cb3d11f91af
commandblock2 <tony_hu@live.com>
2020-10-05 18:27:17 +0800

    Fix SFML link problem.

--- ReaCl/ReaCl.cpp
+++ ReaCl/ReaCl.cpp
@@ -4,10 +4,33 @@
 #include "ReaCl.h"
 #include <box2d/b2_body.h>
 
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
 using namespace std;
 
 int main()
 {
 	b2BodyDef def;
+    
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+        // Update the window
+        window.display();
+    }
+    
 	return 0;
 }
